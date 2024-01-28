f56212224862adaaf652ba70916386a1cc9108be
azurepolarbear <azurepolarbear@users.noreply.github.com>
2024-01-28 00:32:56 -0600

    Add initial files from template

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# gen-art-test_color
+# Generative Art Tests: Color
+
+Copyright (C) 2024 Brittni Watkins
+
 A repository for testing colors in brittni and the polar bear's generative art library.
---
+++ rollup.config.mjs
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2023-2024 Brittni Watkins.
+ *
+ * This file is a part of brittni and the polar bear's Generative Art Project Template,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import eslint from '@rollup/plugin-eslint';
+import terser from '@rollup/plugin-terser';
+import css from 'rollup-plugin-css-only';
+import html from '@rollup/plugin-html';
+import analyzer from 'rollup-plugin-analyzer';
+import serve from 'rollup-plugin-serve';
+import zip from 'rollup-plugin-zip';
+
+import { readFileSync } from 'node:fs';
+
+const dev = process.env.ROLLUP_WATCH === 'true';
+
+export default {
+    input: './src/sketch.ts',
+    output: {
+        dir: './out/dist',
+        format: 'umd',
+        name: 'GenerativeArtTemplate',
+        sourcemap: true,
+        preserveModules: false
+    },
+    plugins: [
+        typescript(),
+        commonjs(),
+        nodeResolve({
+            extensions: ['.ts']
+        }),
+        eslint({
+            include: [
+                './src/**/*.ts'
+            ],
+            throwOnError: true,
+            throwOnWarning: true
+        }),
+        terser(),
+        exportFavicon(),
+        css({
+            output: 'bundle.css'
+        }),
+        html({
+            title: 'Generative Art Template',
+            publicPath: './'
+        }),
+        analyzer({
+            summaryOnly: true
+        }),
+        dev && serve({
+            contentBase: './out/dist',
+            host: '127.0.0.1',
+            port: 8080
+        }),
+        zip({
+            dir: './out/zip'
+        })
+    ]
+};
+
+function exportFavicon() {
+    return {
+        generateBundle() {
+            this.emitFile({
+                type: 'asset',
+                fileName: 'favicon.ico',
+                source: readFileSync('./assets/icon/favicon.ico')
+            });
+        }
+    };
+}
